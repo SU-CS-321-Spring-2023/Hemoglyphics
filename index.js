69291e0fdb7aaf12bf60fd8c3c3f79b4710ed068
Isaac Copenhaver <isaaccopenhaver@gmail.com>
2024-04-08 23:01:12 -0400

    updated user call

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require( "express");
-const {createHash} = require('crypto');
+const express = require("express");
+const { createHash } = require('crypto');
 const cors = require("cors");
 const mysql = require("mysql");
 
@@ -16,43 +16,43 @@ const db = mysql.createPool({
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", (req, res) => {
-  res.send("Hello, world!");
-});
-
-app.get("/user", (req, res) => {
-  const q = "SELECT id FROM userName TheFireGolem";
-  db.query(q, (err, data)=>{
-    if(err) return res.json(err);
-    return res.json(data);
-  });
-});
-
-app.post('/register',(req,res)=>{
-  const pass = req.body.password;
-  const userName = req.body.userName;
-  const email = req.body.email;
-  const salt = makeSalt(16);
-  var hash = salt.concat(pass);
-  hash = createHash('sha256').update(hash).digest('hex');
-  db.query('INSERT INTO userInfo (userName, passWord, salt, email) VALUES(?);', [userName, hash, salt, email], (err, data)=>{
-    if (err) return res.json(err);
-    return res.json("user registered successfully");
-  });
-});
-
 function makeSalt(length) {
   let result = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@!#$&_';
   const charactersLength = characters.length;
   let counter = 0;
   while (counter < length) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      counter += 1;
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
   }
   return result;
 }
 
-app.listen(12000, ()=>{
-  console.log("Server running on port 12000")
+app.get("/test", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.get("/user", (req, res) => {
+  const q = "SELECT id FROM users WHERE userName = 'TheFireGolem';";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post('/register', (req, res) => {
+  const pass = req.body.password;
+  const userName = req.body.userName;
+  const email = req.body.email;
+  const salt = makeSalt(16);
+  var hash = salt.concat(pass);
+  hash = createHash('sha256').update(hash).digest('hex');
+  db.query('INSERT INTO userInfo (userName, passWord, salt, email) VALUES (?, ?, ?, ?);', [userName, hash, salt, email], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("user registered successfully");
+  });
+});
+
+app.listen(12000, () => {
+  console.log("Server running on port 12000");
 });
