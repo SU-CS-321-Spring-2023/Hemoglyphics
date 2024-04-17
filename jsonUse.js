9e14cebb3f26528b08753faaae45912ab41c5f41
ahitchens <ahitchens@stetson.edu>
2024-04-17 16:50:02 -0400

    some predictive cycling

--- jsonUse.js
+++ jsonUse.js
@@ -1,4 +1,28 @@
 const fs = require('fs');
+const moment = require('moment');
+
+function predictCycleLength(periodData, lastPeriodDate) {
+  const lastEntry = periodData[periodData.length - 1];
+  const lastPeriodStartDate = moment(lastEntry.startDate);
+  const currentDate = moment(lastPeriodDate);
+  const elapsedDays = currentDate.diff(lastPeriodStartDate, 'days');
+
+  const averageCycleLength = periodData.reduce((acc, entry) => acc + entry.cycleLength, 0) / periodData.length;
+
+  const predictedCycleLength = Math.round((averageCycleLength * elapsedDays) / lastEntry.cycleLength);
+  
+  return predictedCycleLength;
+}
+
+function predictPeriodLength(periodData) {
+  const averagePeriodLength = periodData.reduce((acc, entry) => acc + entry.periodLength, 0) / periodData.length;
+  return Math.round(averagePeriodLength);
+}
+
+function calculateNextStartDate(lastPeriodDate, predictedCycleLength) {
+  const nextStartDate = moment(lastPeriodDate).add(predictedCycleLength, 'days');
+  return nextStartDate.format("YYYY-MM-DD");
+}
 
 fs.readFile('user_data/cycleData.json', 'utf8', (err, data) => {
   if (err) {
@@ -6,16 +30,19 @@ fs.readFile('user_data/cycleData.json', 'utf8', (err, data) => {
     return;
   }
   const jsonData = JSON.parse(data);
-  console.log('Existing JSON Data:');
-  console.log(jsonData);
+
+  const lastPeriodDate = jsonData.dataEntries[jsonData.dataEntries.length - 1].startDate;
+  const predictedCycle = predictCycleLength(jsonData.dataEntries, lastPeriodDate);
+  const predictedPeriod = predictPeriodLength(jsonData.dataEntries);
+  const nextStartDate = calculateNextStartDate(lastPeriodDate, predictedCycle);
 
   const newData = {
     dataEntries: [
       ...jsonData.dataEntries,
       {
-        startDate: '2024-04-02',
-        periodLength: 3,
-        cycleLength: 28,
+        startDate: nextStartDate,
+        periodLength: predictedPeriod,
+        cycleLength: predictedCycle,
       },
     ],
   };
